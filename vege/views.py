8ef12c7a0d462e898c6d0045c0053a6eb6830f56
Dhirendra Singh Dhami <dhirendraxd@gmail.com>
2024-02-15 00:03:56 +0545

    line error

--- vege/views.py
+++ vege/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import Receipe
 
-# Create your views here.
 def receipes(request):
     if request.method == "POST":
         data = request.POST
-        receipe_image = request.FILES.get('receipe_image')  # Corrected the syntax here
+        receipe_image = request.FILES.get('receipe_image')
         receipe_name = data.get('recipe_name')
         receipe_description = data.get('receipe_description')
 
@@ -15,13 +14,4 @@ def receipes(request):
             receipe_description=receipe_description,
         )
         
-        
-        return redirect('/receipe')
-    
-    
-    
-    queryset=Receipe.object.all()
-    context = {'receipes': queryset}
-
-        
-    return render(request, 'receipes.html' ,context)
+       
